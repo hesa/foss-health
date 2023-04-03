c59b295ce319d69828bd86b85fdc94e73b0296d7
Henrik Sandklef <hesa@sandklef.com>
2023-04-03 22:58:04 +0200

    clean up factory code

--- naive_foss_health/repo_factory.py
+++ naive_foss_health/repo_factory.py
@@ -1,9 +1,23 @@
 import logging
 from naive_foss_health.scrapers.github import GitHubRepoScraper
+from naive_foss_health.repo_interface import RepoScraperException
+
 
 class RepoScraperFactory:
 
+    __supported_scrapers = [ GitHubRepoScraper ]
+
+    @staticmethod
+    def supported_scrapers():
+        return RepoScraperFactory.__supported_scrapers
+    
     @staticmethod
     def RepoScraper(repo):
-        logging.debug("create GitHub Scraper")
-        return GitHubRepoScraper(repo)
+        for scraper in RepoScraperFactory.__supported_scrapers:
+            try:
+                logging.debug(f'trying to create {scraper.repo_provider} Scraper')
+                return scraper(repo)
+            except:
+                logging.debug(f'failed creating {scraper.repo_provider} Scraper')
+
+        raise RepoScraperException(f'Could not find a scraper for: {repo}.\n   We have available scrapers for : {[ x.url_expr() for x in RepoScraperFactory.supported_scrapers() ]}' )
