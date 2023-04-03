6966e382c1c16887f8ecd33ae24fbbac54a3f8fe
Henrik Sandklef <hesa@sandklef.com>
2023-04-03 22:58:19 +0200

    clean up factory code

--- naive_foss_health/repo_interface.py
+++ naive_foss_health/repo_interface.py
@@ -6,3 +6,18 @@ class RepoScraper:
     def scan_repo(self):
         return None
 
+    @staticmethod
+    def url_expr():
+        return None
+
+    @staticmethod
+    def repo_provider():
+        return None
+
+
+class RepoScraperException(Exception):
+
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
+
